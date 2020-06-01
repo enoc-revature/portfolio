3e36a4cba1250791e64da5c147891bed7ed26d75
Enoc Santiago <enocsanti2@gmail.com>
2020-06-01 08:52:31 -0400

    Wrote some tests for benco, worker, bencodetails, workerdetails

--- trms/src/test/java/com/portfilio/trms/test/pages/TestWorkerPage.java
+++ trms/src/test/java/com/portfilio/trms/test/pages/TestWorkerPage.java
@@ -1,6 +1,8 @@
 package com.portfilio.trms.test.pages;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
@@ -12,6 +14,7 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -24,7 +27,6 @@ public class TestWorkerPage {
 	protected static final String TEST_DIRECTORY = System.getenv("TEST_INPUT") + "trms\\";
 	protected static String BASE_URL;
 	protected static List<String> loginCredList;
-
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -58,11 +60,12 @@ public class TestWorkerPage {
 	public void setUp() throws Exception {
 		wd = new ChromeDriver();
 		wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		wd.get(BASE_URL + "login.html");
+		wd.get(BASE_URL + "benco.html");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		wd.close();
 	}
 
 	@Test
@@ -72,21 +75,15 @@ public class TestWorkerPage {
 		assertTrue("Login Page Exists" ,s.equals("Login"));
 	}
 	
+	@Ignore
 	@Test
 	public void workerSessionExists() {
-		
 	}
 	
 	@Test
 	public void workerRowClick() {
-		
-	}
-
-	@Test
-	public void LoginSucceedsToWorkerPage() {
 		WebElement elem = null;
 		WebElement rowClick = null;
-		String username = null;
 
 		try {
 			rowClick = wd.findElement(By.id("submitId"));
@@ -94,14 +91,13 @@ public class TestWorkerPage {
 			fail("Row <tr> element does not exist.");
 		}
 
-
 		rowClick.click();
 
 		try {
-			elem = wd.findElement(By.id("myReimbursementsId"));
+			elem = wd.findElement(By.xpath("/html/body/h1"));
 		} catch (NoSuchElementException e) {
-			fail("<div> element does not exist.");
+			fail("<h1> element does not exist.");
 		}
-		assertEquals("div", elem.getTagName());
+		assertEquals("Reimbursement Details", elem.getAttribute("innerHTML"));
 	}
 }
